3924e97236c62fd5cc145d5f2280f21f08ae3fe5
Joey360 <Joey360@users.noreply.github.com>
2017-03-21 13:51:10 -0700

    Update strand.py

--- strand.py
+++ strand.py
@@ -8,7 +8,7 @@ led_dma = 5   #DMA channel
 led_brightness = 255 # maximum (8 bit)
 Led_Invert = False # True inverts signal (for NPN transistors)
        
-if __name__ = '__main__;:
+if __name__ = '__main__':
     strip = Adafruit_NeoPixel(led_count, led_pin, led_freq_Hz, led_dma, led_invert, led_brightness)
     strip.begin()
         for i in strip.numPixels:
