9a412919bdc73d20fbf070e2368e35ecd0f0ae19
Joey360 <Joey360@users.noreply.github.com>
2017-03-21 13:50:52 -0700

    Add files via upload

---
+++ strand.py
@@ -0,0 +1,17 @@
+import time
+from neopixel import *
+
+led_count = 24
+led_pin = 18
+led_freq_Hz = 800000 # (800KHz)
+led_dma = 5   #DMA channel
+led_brightness = 255 # maximum (8 bit)
+Led_Invert = False # True inverts signal (for NPN transistors)
+       
+if __name__ = '__main__;:
+    strip = Adafruit_NeoPixel(led_count, led_pin, led_freq_Hz, led_dma, led_invert, led_brightness)
+    strip.begin()
+        for i in strip.numPixels:
+            strip.setPixelColor(i, 0, 0, 255))
+            if i == strip.numPixels:
+                strip.show()
