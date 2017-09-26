445192210480733ce87185083ddc9c07e5194d25
Joey360 <Joey360@users.noreply.github.com>
2017-09-25 19:10:30 -0700

    Calculate distance of object
    Add code

---
+++ Distance.py
@@ -0,0 +1,7 @@
+Width = 0
+Distance = 0
+Pixels = 0
+# Find Pixels using bounding box
+# then use F = (P*D)/W (Or height)
+# With the focal point then use D' = (W*F)/P Units are in inches
+# Then find offset from center of focal point
